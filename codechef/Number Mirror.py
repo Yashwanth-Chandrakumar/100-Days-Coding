51a6a188bf2f5957ce873d92524dfdbc017552bd
Yashwanth S C <121328223+Yashwanth-Chandrakumar@users.noreply.github.com>
2023-10-12 19:21:59 +0530

    Create Number Mirror.py

---
+++ codechef/Number Mirror.py
@@ -0,0 +1,28 @@
+Problem
+Write a program that takes a number 
+N as the input, and prints it to the output.
+
+Input Format
+The only line of input contains a single integer.
+
+Output Format
+Output the answer in a single line.
+
+Sample 1:
+Input
+Output
+123
+123
+Explanation:
+The input is 123. So the output is also 123.
+
+Sample 2:
+Input
+Output
+15
+15
+
+
+Answer:
+n = int(input())
+print(n)
