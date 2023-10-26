39cea73d5cb3d10237c8fdca8cea88ac136b0504
Yashwanth S C <yashwanth2k05@gmail.com>
2023-10-26 20:26:01 +0530

    Create Dominant elements.cpp

---
+++ codechef/Dominant elements.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int j=0;j<t;j++)
+    {
+       int n,maxcount=0,count=0;
+        bool r=false;
+        cin>>n;
+        int a[n];
+         for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        sort(a,a+n);
+        for(int i=0;i<n;i++)
+        {
+            count++;
+            if((a[i]!=a[i+1])||(i==n-1))
+            {
+                if(count>maxcount)
+                {
+                    maxcount=count;
+                    r=true;
+                }
+                else if(count==maxcount)
+                {
+                    r=false;
+                }
+                count=0;
+            }
+        }
+        if(r==true)
+        {
+            cout<<"\nYES";
+        }
+        else
+        {
+            cout<<"\nNO";
+        }
+    }
+    return 0;
+}
