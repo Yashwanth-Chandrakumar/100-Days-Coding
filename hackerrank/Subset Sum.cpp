df84d0be8df02d33556a964744724f0167ffee32
Yashwanth S C <yashwanth2k05@gmail.com>
2023-11-08 20:56:20 +0530

    Create Subset Sum.cpp

---
+++ hackerrank/Subset Sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<vector<bool>> dp(n + 1, vector<bool>(k + 1, false));
+
+    for (int i = 0; i <= n; i++) {
+        dp[i][0] = true;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= k; j++) {
+            if (arr[i - 1] <= j) {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[n][k];
+}
