69adf3b779785d8c22b75168b9fa07ec9456b619
Yashwanth S C <yashwanth2k05@gmail.com>
2023-11-21 18:39:05 +0530

    Create Chocolates.java

---
+++ codechef/Chocolates.java
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+	int n,k,a,b;
+	cin>>n;
+	cout<<n-2<<" "<<1<<" "<<1<<endl;
+	
+    }
+	return 0;
+}
+
