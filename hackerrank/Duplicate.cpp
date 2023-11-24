50435e46a265fb786f39d1dce1c4c8b4908d55a2
Yashwanth S C <yashwanth2k05@gmail.com>
2023-11-24 19:35:33 +0530

    Create Duplicate.cpp

---
+++ hackerrank/Duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>ans;
+        for(int i=0;i<nums.size();i++){
+            ans[nums[i]]+=1;
+        }
+        for(const auto&pair : ans){
+            if(pair.second>1){
+                return true;
+                break;
+            }
+        }
+        return false;
+
+    }
+};
