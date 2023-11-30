9e330b631ca5b09d9476c74d9ccc6ec858d28928
Yashwanth S C <yashwanth2k05@gmail.com>
2023-11-30 09:59:31 +0530

    Create Pascals Triangle.cpp

---
+++ leetcode/Pascals Triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        vector<int> prevRow;
+        
+        for (int i = 0; i < numRows; i++) {
+            vector<int> currentRow(i + 1, 1);
+            
+            for (int j = 1; j < i; j++) {
+                currentRow[j] = prevRow[j - 1] + prevRow[j];
+            }
+            
+            result.push_back(currentRow);
+            prevRow = currentRow;
+        }
+        
+        return result;
+    }
+};
