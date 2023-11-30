f6a556cf7eecd2fbb466e9672a85907451b23cef
Yashwanth S C <yashwanth2k05@gmail.com>
2023-11-30 10:45:29 +0530

    Create Vowel reversal.cpp

---
+++ hackerearth/Vowel reversal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        string word = s;
+        int start = 0;
+        int end = s.length() - 1;
+        string vowels = "aeiouAEIOU";
+        
+        while (start < end) {
+            while (start < end && vowels.find(word[start]) == string::npos) {
+                start++;
+            }
+            
+            while (start < end && vowels.find(word[end]) == string::npos) {
+                end--;
+            }
+            
+            swap(word[start], word[end]);
+            
+            start++;
+            end--;
+        }
+        
+        return word;
+    }
+};
