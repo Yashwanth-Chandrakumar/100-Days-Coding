56cdca4fd77a4ec68915b2c1a9b49861c7f97666
Yashwanth S C <yashwanth2k05@gmail.com>
2023-12-04 21:09:05 +0530

    Create Linked List triplets.cpp

---
+++ hackerearth/Linked List triplets.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+/***********************************************
+
+    Following is the class structure of the Node class:
+
+    class DLLNode
+    {
+        public:
+        int data;
+        DLLNode *next;
+        DLLNode *prev;
+    };
+
+***********************************************/
+
+int countTriplets(DLLNode* head, int x) {
+   DLLNode* ptr1, *ptr2, *ptr3;
+    int count = 0;
+
+    for (ptr1 = head; ptr1 != NULL; ptr1 = ptr1->next)
+        for (ptr2 = ptr1->next; ptr2 != NULL; ptr2 = ptr2->next)
+            for (ptr3 = ptr2->next; ptr3 != NULL; ptr3 = ptr3->next)
+                if (ptr1->data + ptr2->data + ptr3->data == x)
+                    count++;
+
+    return count;
+}
