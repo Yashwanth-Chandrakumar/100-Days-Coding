cda8ed048d1acf25369446ad5320e232315d4751
Yashwanth S C <yashwanth2k05@gmail.com>
2023-12-04 20:47:05 +0530

    Create Easy permutation.cpp

---
+++ codechef/Easy permutation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+		int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    for(int i=n;i>0;i--){
+	        cout<<i<<" ";
+	    }
+	}
+	return 0;
+}
