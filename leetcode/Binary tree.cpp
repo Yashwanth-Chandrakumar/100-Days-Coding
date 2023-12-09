64c008e3afaca153658ce466b2ed082df9901756
Yashwanth S C <yashwanth2k05@gmail.com>
2023-12-09 17:09:38 +0530

    Create Binary tree.cpp

---
+++ leetcode/Binary tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        helper(root, result);
+        return result;
+    }
+
+    void helper(TreeNode* root, vector<int>& result) {
+        if (root != nullptr) {
+            helper(root->left, result);
+            result.push_back(root->val);
+            helper(root->right, result);
+        }
+    }
+};
