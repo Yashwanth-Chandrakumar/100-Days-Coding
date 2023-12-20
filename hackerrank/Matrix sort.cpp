58f9326cfdbe82611cc1e08dbc8eaf2767d53b77
Yashwanth S C <yashwanth2k05@gmail.com>
2023-12-20 21:48:55 +0530

    Create Matrix sort.cpp

---
+++ hackerrank/Matrix sort.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int arr[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        sort(arr[i],arr[i]+m);
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
