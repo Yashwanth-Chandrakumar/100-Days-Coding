dd4df490237a9ed648bd6a387c61d8fe34ca1b02
Yashwanth S C <yashwanth2k05@gmail.com>
2023-12-25 21:13:55 +0530

    Create Group Anagrams.py

---
+++ hackerrank/Group Anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
