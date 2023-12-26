12b84e7ebd0ac92cc7df7d9a56c7c94aa081e4d5
Yashwanth S C <yashwanth2k05@gmail.com>
2023-12-26 17:59:40 +0530

    Create Minimum insertions to make a string palindrome.cpp

---
+++ hackerearth/Minimum insertions to make a string palindrome.cpp
@@ -0,0 +1,18 @@
+int minimumInsertions(string &str)
+{
+	int n = str.length();
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+
+    for (int len = 2; len <= n; len++) {
+        for (int i = 0; i <= n - len; i++) {
+            int j = i + len - 1;
+            if (str[i] == str[j]) {
+                dp[i][j] = dp[i + 1][j - 1];
+            } else {
+                dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[0][n - 1];
+}
