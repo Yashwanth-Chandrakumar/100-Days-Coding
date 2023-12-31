5b4e1462143bdbd35f4fbde900fd1281fd382fb5
Yashwanth S C <yashwanth2k05@gmail.com>
2023-12-31 15:33:07 +0530

    Create Largest Rectangle.py

---
+++ hackerearth/Largest Rectangle.py
@@ -0,0 +1,21 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = []
+        max_area = 0
+
+        for i in range(len(heights)):
+            while stack and heights[i] < heights[stack[-1]]:
+                height = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                max_area = max(max_area, height * width)
+
+            stack.append(i)
+
+        while stack:
+            height = heights[stack.pop()]
+            width = len(heights) if not stack else len(heights) - stack[-1] - 1
+            max_area = max(max_area, height * width)
+
+        return max_area
+
+        
