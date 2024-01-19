325f8e92810aa0b4eecc448ff13d707322a286b8
Yashwanth S C <yashwanth2k05@gmail.com>
2024-01-19 17:44:44 +0530

    Create Array Concat.py

---
+++ hackerearth/Array Concat.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans=[]
+        for i in nums:
+            ans.append(i)
+        for i in nums:
+            ans.append(i)
+        return ans
+        
